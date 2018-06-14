d5d06e10249c1e154c1e76dd82e93f39d4e81ee4
guanxy <913367245@qq.com>
2018-06-15 00:12:46 +0800

    更新Day31-33_Task

--- 基础学院/Day31-33_Task/js/checkbox.js
+++ 基础学院/Day31-33_Task/js/checkbox.js
@@ -1,53 +1,112 @@
 /*
-* @Author: guanxy
-* @Date:   2018-06-06 12:06:56
-* @Last Modified by:   guanxy
-* @Last Modified time: 2018-06-13 00:12:56
-*/
+ * @Author: guanxy
+ * @Date:   2018-06-06 12:06:56
+ * @Last Modified by:   guanxy
+ * @Last Modified time: 2018-06-15 00:12:01
+ */
 window.onload = function() {
     var region_select_div = document.getElementById('region-select');
     var product_select_div = document.getElementById('product-select');
     var div_table_box = document.getElementById('table-wrapper');
-    var list=[];
-    var input =region_select_div.getElementsByTagName('input');
-    for(var i=0;i<input.length;i++){
-    	change(input[i]);
-    	console.log(input[i]);
+    var list = [],
+        rCArr = [],
+        pCArr = [],
+        box = [];
+    var regionInput = region_select_div.getElementsByTagName('input');
+    var productInput = product_select_div.getElementsByTagName('input');
+    eleChange(regionInput);
+    eleChange(productInput);
+
+
+    function eleChange(ele) {
+        for (var i = 0; i < ele.length; i++) {
+            change(ele[i], ele);
+        }
     }
-    function change(ev){
+
+    function change(ev, input) {
         ev.onchange = function() {
-        var selectVal=ev.value;
-        if(ev.type==="checkbox"){
-           var checkType=ev.getAttribute("checkbox-type");
-           if(checkType==="all"){
-           	  console.log("aaa");
-           	  for(var i=1;i<input.length;i++){
-           	  	input[i].checked="checked";
-           	  }
-           }else{
-           	 console.log("bbb");
-           	
-           }
+            var selectVal = ev.value;
+            if (ev.type === "checkbox") {
+                var checkType = ev.getAttribute("checkbox-type");
+                if (checkType === "all") {
+                    if (ev.checked) {
+                        for (var i = 1; i < input.length; i++) {
+                            input[i].checked = "checked";
+                        }
+                        input == regionInput ? rCArr = getCheckVal(input, box) : pCArr = getCheckVal(input, box);
+                        // createTable(setTableData());
+                        div_table_box.innerHTML = null;
+                        div_table_box.appendChild(createTable(setTableData()));
+                    }
+
+                } else {
+                    var c = 0;
+                    for (var i = 1; i < input.length; i++) {
+                        if (input[i].checked) {
+                            c++;
+                        }
+                    }
+                    if (c == input.length - 1) {
+                        input[0].checked = "checked";
+
+
+                    } else {
+                        input[0].checked = false;
+                    }
+                    input == regionInput ? rCArr = getCheckVal(input, box) : pCArr = getCheckVal(input, box);
+                  
+                    div_table_box.innerHTML = null;
+                    div_table_box.appendChild(createTable(setTableData()));
+                    
+                }
+            }
         }
-     }
+
+
     }
-    function getRegionSelectVal(region,product) {
-        list=[];
-        if(product.length!==0){
-           for(var l = 0; l <sourceData.length; l++){
-            if (sourceData[l].region === region && sourceData[l].product ===product) {
-                    list.push(sourceData[l]);
+
+    function getCheckVal(arr, checkedArrlist) {
+        checkedArrlist = [];
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i].checked) {
+                checkedArrlist.push(arr[i].value);
             }
-        } 
-        }else{
-            for(var l = 0; l <sourceData.length; l++){
-            if (sourceData[l].region === region) {
-               list.push(sourceData[l]);
-            }
-         }  
         }
-        
-        return list;
+        return checkedArrlist;
+    }
+
+    function setTableData() {
+        console.log(rCArr);
+        console.log(pCArr);
+        var data = [],
+            databox = [];
+        if (rCArr.length != 0) {
+            if (pCArr.length != 0) {
+                for (var r = 0; r < rCArr.length; r++) {
+                    for (var s = 0; s < sourceData.length; s++) {
+                        if (rCArr[r] == sourceData[s].region) {
+                            databox.push(sourceData[s]);
+
+                        }
+                    }
+                }
+                console.log("databox");
+                console.log(databox);
+                for (var p = 0; p < pCArr.length; p++) {
+                    for (var s = 0; s < databox.length; s++) {
+                        if (pCArr[p] == databox[s].product) {
+                            data.push(databox[s]);
+
+                        }
+                    }
+                }
+                console.log("结果数据");
+                console.log(data);
+            }
+
+        }
+        return data;
     }
 
 
@@ -65,21 +124,21 @@ window.onload = function() {
         }
         table.appendChild(tr);
         for (var l = 0; l < data.length; l++) {
-                var tbodyData = [];
-                tbodyData.push(data[l].product);
-                tbodyData.push(data[l].region);
-                for (var k = 0; k < data[l].sale.length; k++) {
-                    tbodyData.push(data[l].sale[k]);
-                }
-                var tr = document.createElement('tr');
-                for (var j = 0; j < tbodyData.length; j++) {
-                    var td = document.createElement("td");
-                    td.innerHTML = tbodyData[j];
-                    tr.appendChild(td);
-                }
+            var tbodyData = [];
+            tbodyData.push(data[l].product);
+            tbodyData.push(data[l].region);
+            for (var k = 0; k < data[l].sale.length; k++) {
+                tbodyData.push(data[l].sale[k]);
+            }
+            var tr = document.createElement('tr');
+            for (var j = 0; j < tbodyData.length; j++) {
+                var td = document.createElement("td");
+                td.innerHTML = tbodyData[j];
+                tr.appendChild(td);
+            }
 
-                table.appendChild(tr);
-            
+            table.appendChild(tr);
+
 
         }
 
